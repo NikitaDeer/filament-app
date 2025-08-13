3945322baef0ad606eab2a849c96dc6a1a88ac84
Nikita Deer <nikita.deer@gmail.com>
2025-08-13 14:50:11 +0300

    Обновили форму и таблицу пользователей: добавили обязательные поля, улучшили валидацию, изменили структуру колонок и удалили неиспользуемые поля и методы из модели User для оптимизации кода.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->bigInteger('telegram_id')->nullable()->unique();
-            $table->boolean('trial_used')->default(false);
-            $table->timestamp('trial_ends_at')->nullable();
-            $table->foreignId('current_tariff_id')->nullable()->constrained('tariffs')->nullOnDelete();
-            $table->enum('tariff_status', ['active', 'expired', 'paused'])->default('expired');
             $table->rememberToken();
             $table->timestamps();
         });
